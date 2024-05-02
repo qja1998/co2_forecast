99535993886e84984adc1e7726886961abcb14cc
kwon <qja1998@naver.com>
2024-05-02 16:00:49 +0900

    arima train

--- ARIMA/dataset.py
+++ ARIMA/dataset.py
@@ -1,21 +1,6 @@
-import torch
 import numpy as np
 
-
-# if window is 100 and prediction step is 1
-# in -> [0..99]
-# target -> [1..100]
-def _create_input_sequences(input_data, tw, output_window):
-    inout_seq = []
-    L = len(input_data)
-    for i in range(L-tw):
-        train_seq = np.append(input_data[i:i+tw][:-output_window] , output_window * [0])
-        train_label = input_data[i:i+tw]
-        #train_label = input_data[i+output_window:i+tw+output_window]
-        inout_seq.append((train_seq ,train_label))
-    return torch.FloatTensor(inout_seq)
-
-def get_data(df, input_window, output_window):
+def get_data(df):
     time        = np.arange(0, 400, 0.1)
     #amplitude   = np.sin(time) + np.sin(time*0.05) +np.sin(time*0.12) *np.random.normal(-0.2, 0.2, len(time))
     
@@ -27,21 +12,4 @@ def get_data(df, input_window, output_window):
     train_data = amplitude[:sampels]
     test_data = amplitude[sampels:]
 
-    # convert our train data into a pytorch train tensor
-    #train_tensor = torch.FloatTensor(train_data).view(-1)
-    # todo: add comment.. 
-    train_sequence = _create_input_sequences(train_data, input_window + output_window, output_window)
-    # train_sequence = train_sequence[:-output_window] #todo: fix hack?
-
-    #test_data = torch.FloatTensor(test_data).view(-1) 
-    test_data = _create_input_sequences(test_data, input_window + output_window, output_window)
-    # test_data = test_data[:-output_window] #todo: fix hack?
-
-    return train_sequence, test_data, scaler
-
-def get_batch(source, i, batch_size, input_window):
-    seq_len = min(batch_size, len(source) - 1 - i)
-    data = source[i:i+seq_len]    
-    input = torch.stack(torch.stack([item[0] for item in data]).chunk(input_window,1)) # 1 is feature size
-    target = torch.stack(torch.stack([item[1] for item in data]).chunk(input_window,1))
-    return input, target
+    return train_data, test_data, scaler
--- ARIMA/util.py
+++ ARIMA/util.py
@@ -1,14 +1,25 @@
 import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
 
-def plot_ac(series, diff=None):
-    if diff:
-        series = series.diff(periods=diff).iloc[1:]
-    plt.title(f"Series (diff{diff})")
-    series.plot()
-    plt.title(f"ACF (diff{diff})")
-    plot_acf(series)
-    plt.title(f"PACF (diff{diff})")
-    plot_pacf(series)
+def plot_ac(series, t, diff=[]):
+    fig, ax = plt.subplots(3, 1, figsize=(10, 12))
+
+
+    series.plot(ax=ax[0])
+    plot_acf(series, ax=ax[1])
+    plot_pacf(series, ax=ax[2])
+
+    plt.title(f"[{t}] original")
+
+    for d in diff:
+        fig, ax = plt.subplots(3, 1, figsize=(10, 12))
+        series_diff = series.diff(periods=d).iloc[1:]
+        
+        series_diff.plot(ax=ax[0])
+        plot_acf(series_diff, ax=ax[1])
+        plot_pacf(series_diff, ax=ax[2])
+
+        plt.title(f"[{t}] diff : {d}")
+    
     plt.show()
     
