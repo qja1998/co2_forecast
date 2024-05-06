b5c69ab3ee4ea8f101ad5e3ef5215bd452c9d09d
qja1998 <62369107+qja1998@users.noreply.github.com>
2024-05-06 12:05:45 +0900

    arima test

--- ARIMA/util.py
+++ ARIMA/util.py
@@ -1,25 +1,32 @@
 import matplotlib.pyplot as plt
-from statsmodels.graphics.tsaplots import plot_pacf, plot_acf
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.stattools import acf, pacf
 
-def plot_ac(series, t, diff=[]):
-    fig, ax = plt.subplots(3, 1, figsize=(10, 12))
-
-
-    series.plot(ax=ax[0])
-    plot_acf(series, ax=ax[1])
-    plot_pacf(series, ax=ax[2])
-
-    plt.title(f"[{t}] original")
+def plot_slect(series, t, data, acf, pacf, d=None):
+    sub_plot_size = sum([data, acf, pacf])
+    fig, ax = plt.subplots(sub_plot_size, 1, figsize=(10, sub_plot_size * 4))
+    pos = 0
+    if d is not None:
+        series = series.diff(periods=d).iloc[1:]
+    if data:
+        series.plot(ax=ax[pos])
+        pos += 1
+    if acf:
+        plot_acf(series, ax=ax[pos])
+        pos += 1
+    if pacf:
+        plot_pacf(series, ax=ax[pos])
+        pos += 1
+        
+    plt.title(f"[{t}] diff : {d}")
+    
+    
+def plot_ac(series, t, diff=[], data=True, acf=True, pacf=True):
+    
+    plot_slect(series, t, data, acf, pacf)
 
     for d in diff:
-        fig, ax = plt.subplots(3, 1, figsize=(10, 12))
-        series_diff = series.diff(periods=d).iloc[1:]
-        
-        series_diff.plot(ax=ax[0])
-        plot_acf(series_diff, ax=ax[1])
-        plot_pacf(series_diff, ax=ax[2])
+        plot_slect(series, t, data, acf, pacf, d)
 
-        plt.title(f"[{t}] diff : {d}")
-    
     plt.show()
     
