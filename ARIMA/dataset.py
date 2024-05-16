7ec2aad6d5f45a391f55236c85a80dc312d2eebe
kwon <qja1998@naver.com>
2024-05-16 18:43:08 +0900

    auto arima

--- ARIMA/dataset.py
+++ ARIMA/dataset.py
@@ -1,12 +1,17 @@
 import numpy as np
 
-def get_data(df):
+def get_data(df, scaling=True):
     time        = np.arange(0, 400, 0.1)
     #amplitude   = np.sin(time) + np.sin(time*0.05) +np.sin(time*0.12) *np.random.normal(-0.2, 0.2, len(time))
     
-    from sklearn.preprocessing import MinMaxScaler
-    scaler = MinMaxScaler() 
-    amplitude = scaler.fit_transform(df.to_numpy().reshape(-1, 1)).reshape(-1)
+    scaler = None
+
+    if scaling:
+        from sklearn.preprocessing import MinMaxScaler
+        scaler = MinMaxScaler() 
+        amplitude = scaler.fit_transform(df.to_numpy().reshape(-1, 1)).reshape(-1)
+    else:
+        amplitude = df.to_numpy()
     
     sampels = int(len(amplitude) * 0.8)
     train_data = amplitude[:sampels]
--- ARIMA/util.py
+++ ARIMA/util.py
@@ -2,31 +2,35 @@ import matplotlib.pyplot as plt
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 from statsmodels.tsa.stattools import acf, pacf
 
-def plot_slect(series, t, data, acf, pacf, d=None):
+def plot_slect(series, t, data, acf, pacf, lags, d=None):
     sub_plot_size = sum([data, acf, pacf])
-    fig, ax = plt.subplots(sub_plot_size, 1, figsize=(10, sub_plot_size * 4))
+    _, ax = plt.subplots(sub_plot_size, 1, figsize=(15, sub_plot_size * 4))
+    
     pos = 0
     if d is not None:
-        series = series.diff(periods=d).iloc[1:]
+        series_tmp = series.diff(periods=d).dropna()
+    else:
+        series_tmp = series.copy()
+
     if data:
-        series.plot(ax=ax[pos])
+        series_tmp.plot(ax=ax[pos])
         pos += 1
     if acf:
-        plot_acf(series, ax=ax[pos])
+        plot_acf(series_tmp, ax=ax[pos], lags=lags)
         pos += 1
     if pacf:
-        plot_pacf(series, ax=ax[pos])
+        plot_pacf(series_tmp, ax=ax[pos], lags=lags)
         pos += 1
         
     plt.title(f"[{t}] diff : {d}")
     
     
-def plot_ac(series, t, diff=[], data=True, acf=True, pacf=True):
+def plot_ac(series, t, diff=[], data=True, acf=True, pacf=True, lags=1):
     
-    plot_slect(series, t, data, acf, pacf)
+    plot_slect(series, t, data, acf, pacf, lags)
 
     for d in diff:
-        plot_slect(series, t, data, acf, pacf, d)
+        plot_slect(series, t, data, acf, pacf, lags, d)
 
     plt.show()
     
