618a7b86bc3844ea49d544db3cf1e53854a93db3
kwon <qja1998@naver.com>
2024-06-04 23:27:05 +0900

    transformer full demo

--- transformer/test_notebook/transformer_type/model.py
+++ transformer/test_notebook/transformer_type/model.py
@@ -63,6 +63,57 @@ class TransAm(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
+class TransformerFull(nn.Module):
+    # Constructor
+    def __init__( self, num_tokens=60, dim_model=256, num_heads=8, num_encoder_layers=3, num_decoder_layers=3, dropout_p=0.1, ):
+        super().__init__()
+
+        # INFO
+        self.model_type = "Transformer"
+        self.dim_model = dim_model
+
+        # LAYERS
+        self.positional_encoder = PositionalEncoding(dim_model=dim_model, dropout_p=dropout_p, max_len=5000)
+        self.embedding = nn.Embedding(num_tokens, dim_model)
+        self.transformer = nn.Transformer(
+            d_model=dim_model,
+            nhead=num_heads,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
+            dropout=dropout_p,
+        )
+        self.out = nn.Linear(dim_model, num_tokens)
+
+    def forward(self, src, tgt, tgt_mask=None, src_pad_mask=None, tgt_pad_mask=None):
+        # Src, Tgt size 는 반드시 (batch_size, src sequence length) 여야 합니다.
+
+        # Embedding + positional encoding - Out size = (batch_size, sequence length, dim_model)
+        src = self.embedding(src) * math.sqrt(self.dim_model)
+        tgt = self.embedding(tgt) * math.sqrt(self.dim_model)
+        src = self.positional_encoder(src)
+        tgt = self.positional_encoder(tgt)
+
+        src = src.permute(1,0,2)
+        tgt = tgt.permute(1,0,2)
+
+        # Transformer blocks - Out size = (sequence length, batch_size, num_tokens)
+        transformer_out = self.transformer(src, tgt, tgt_mask=tgt_mask, src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=tgt_pad_mask)
+        out = self.out(transformer_out)
+
+        return out
+
+    def get_tgt_mask(self, size) -> torch.tensor:
+        mask = torch.tril(torch.ones(size, size) == 1) # Lower triangular matrix
+        mask = mask.float()
+        mask = mask.masked_fill(mask == 0, float('-inf')) # Convert zeros to -inf
+        mask = mask.masked_fill(mask == 1, float(0.0)) # Convert ones to 0
+
+        return mask
+
+    def create_pad_mask(self, matrix: torch.tensor, pad_token: int) -> torch.tensor:
+        return (matrix == pad_token)
+
+
 # class TransAm(nn.Module):
 #     def __init__(self, num_tokens, dim_model, num_heads, num_encoder_layers, num_decoder_layers, dropout):
 #         super().__init__()
@@ -174,19 +225,19 @@ def predict_future(eval_models, data_source_list, epoch, steps, input_window, ou
                 steps = output_window
 
             else:
-                split_id = 1
+                split_id = 0
                 data_list = list(data.squeeze().squeeze().cpu())
                 for i, d in enumerate(data_list[::-1]):
                     if d == SEP:
                         split_id = len(data_list) - i
                         break
                 pred_output = []
-                
+
                 for _ in range(0, steps):
                     output = eval_model(data)
                     pred_output.append(output[-1].squeeze().squeeze().cpu())
                     data = data.squeeze().squeeze()
-                    next_seq = np.append(np.array(data[split_id:-2].cpu()), np.array(output[-1].cpu()))
+                    next_seq = np.append(np.array(data[split_id + 1:-2].cpu()), np.array(output[-1].cpu()))
                     pred_seq = next_seq.copy()
                     if diff:
                         diff_seq = np.append(np.diff(next_seq), np.array([SEP]))
@@ -201,7 +252,7 @@ def predict_future(eval_models, data_source_list, epoch, steps, input_window, ou
         true_future = np.array([data_source[i][1][0].cpu() for i in range(true_val_len)])
         true_val = np.array([data_source[i][1][0].cpu() for i in range(true_val_len)])
         
-        data_tensor = torch.Tensor(pred_output[-steps:])
+        data_tensor = torch.Tensor(pred_output)
         data_true_future = torch.Tensor(true_future)
         mse_score = mse(data_tensor, data_true_future[-steps:].squeeze())
         mae_score = mae(data_tensor, data_true_future[-steps:].squeeze())
